4989b67b4ecbde810e64121bcce311728c611fbb
zijianwu <zijian_wu@foxmail.com>
2020-10-08 22:58:54 +0800

    first commit

---
+++ src/cn/edu/ecnu/sort/Selection.java
@@ -0,0 +1,49 @@
+package cn.edu.ecnu.sort;
+
+/**
+ * 选择排序
+ * 比较次数：N^2/2
+ * 交换次数：N
+ */
+public class Selection {
+    public static void sort(Comparable[] a) {
+        int N = a.length;
+        for (int i = 0; i < N; i++) {
+            int min = i;
+            for (int j = i; j < N; j++)
+                if (less(a[j], a[min])) min = j;
+            exch(a, i, min);
+        }
+    }
+
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    private static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; i++)
+            System.out.print(a[i] + " ");
+        System.out.println();
+    }
+
+    public static boolean isSorted(Comparable[] a) {
+        for (int i = 0; i < a.length - 1; i++) {
+            if (!less(a[i], a[i + 1]))
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Integer[] a = {3, 1, 2, 5, 4};
+        sort(a);
+        assert isSorted(a);
+        show(a);
+    }
+}
